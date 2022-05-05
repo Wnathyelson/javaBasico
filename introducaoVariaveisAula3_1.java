0f94c1e713af8c6b683db54e1ecec8a6aed4b567
Wnathyelson <wnathyelson@gmail.com>
2022-05-05 15:54:06 -0300

    Novos codigos

---
+++ introducaoVariaveisAula3_1.java
@@ -0,0 +1,17 @@
+package home.nathyelson.programacaoBasicaJava;
+
+public class introducaoVariaveisAula3_1 {
+
+    public static void main(String [] args) {
+
+            int idade =22;
+            String nome = "Nathyelson";
+            String nomeDoMeuCachorro = "Menino";
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Nome do cachorro: "+ nomeDoMeuCachorro);
+
+    }
+
+}
---
+++ tiposPrimitivosAula3_2.java
@@ -0,0 +1,9 @@
+package home.nathyelson.programacaoBasicaJava;
+
+public class tiposPrimitivosAula3_2 {
+
+    public static void main(String [] args){
+
+
+    }
+}
